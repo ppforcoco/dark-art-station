7a1d2e7bec910c01e009b3ffc21fd70804e9ef8d
ppforcoco <jaanrabia006@gmail.com>
2026-03-23 12:33:58 +0500

    done

--- components/ScrollReset.tsx
+++ components/ScrollReset.tsx
@@ -2,6 +2,8 @@
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
+// Scrolls to top on every page navigation.
+// Next.js App Router doesn't reset scroll automatically for same-layout routes.
 export default function ScrollReset() {
   const pathname = usePathname();
   useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
