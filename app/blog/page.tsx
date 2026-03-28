e1041e34c44f5817d1966d15727905a0d9e1626a
ppforcoco <jaanrabia006@gmail.com>
2026-03-28 14:56:24 +0500

    hmm pull back to 2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -62,7 +62,7 @@ export default async function BlogPage() {
   }, {});
 
   return (
-    <main className="blog-index-page">
+    <main className="blog-index-page" data-force-blog="true">
 
       <nav className="blog-topnav">
         <div className="blog-topnav-inner">
